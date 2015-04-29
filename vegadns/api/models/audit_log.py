5c76562a0f0e016f342b1665e3951a3b1f9ca54d
Bill Shupp <hostmaster@shupp.org>
2015-04-29 14:27:52 -0700

    Added DefaultRecord and AuditLog models

---
+++ vegadns/api/models/audit_log.py
@@ -0,0 +1,15 @@
+from peewee import *
+
+from vegadns.api.models import database, BaseModel
+
+
+class AuditLog(BaseModel):
+    email = CharField(db_column='Email')
+    name = CharField(db_column='Name')
+    cid = IntegerField()
+    domain_id = IntegerField(db_column='domain_id')
+    entry = CharField()
+    time = IntegerField()
+
+    class Meta:
+        db_table = 'log'
---
+++ vegadns/api/models/default_record.py
@@ -0,0 +1,23 @@
+from peewee import *
+
+from vegadns.api.models import database, BaseModel
+
+
+class DefaultRecord(BaseModel):
+    default_type = CharField()
+    distance = IntegerField(null=True)
+    group_owner_id = IntegerField(db_column='group_owner_id', null=True)
+    host = CharField()
+    port = IntegerField(null=True)
+    record_id = IntegerField(
+        db_column='record_id',
+        unique=True,
+        primary_key=True
+    )
+    ttl = IntegerField()
+    type = CharField(null=True)
+    val = CharField(null=True)
+    weight = IntegerField(null=True)
+
+    class Meta:
+        db_table = 'default_records'
