0ea1321cb45024eaa1239a62749a7ad9ca3e99b3
Bill Shupp <hostmaster@shupp.org>
2015-05-11 15:08:19 -0700

    Bind ensure_validation to models.BaseModel 'save' and 'create' methods

--- vegadns/api/models/__init__.py
+++ vegadns/api/models/__init__.py
@@ -14,16 +14,28 @@ database = MySQLDatabase(
 )
 
 
+def ensure_validation(func):
+    def run_validation(self, *args, **kwargs):
+        self.validate()
+        return func(self, *args, **kwargs)
+    return run_validation
+
+
 class BaseModel(Model):
     def to_dict(self):
         return model_to_dict(self)
 
+    @ensure_validation
+    def save(self, *args, **kwargs):
+        # print self
+        super(BaseModel, self).save(*args, **kwargs)
+
+    @ensure_validation
+    def create(self, *args, **kwargs):
+        super(BaseModel, self).create(*args, **kwargs)
+
+    def validate(self):
+        raise Exception('No validation defined')
+
     class Meta:
         database = database
-
-
-def ensure_validation(func):
-    def run_validation(cls):
-        cls.validate()
-        return func()
-    return run_validation
