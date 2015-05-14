0c86475f7857b734e62c7a920c7c7aad6b2189bd
Bill Shupp <hostmaster@shupp.org>
2015-05-13 18:18:39 -0700

    OAuth Tokens
    * Added token endpoint
    * Added oauth.token_expire_time, defaults to 3600

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from vegadns.api import endpoint, app
 from vegadns.api.endpoints import domain, domains, record, records, export
 from vegadns.api.endpoints import apikeys, apikey
+from vegadns.api.endpoints import token
 
 
 if __name__ == '__main__':
---
+++ vegadns/api/endpoints/token.py
@@ -0,0 +1,88 @@
+import time
+import uuid
+
+import peewee
+
+from flask import Flask, abort, request
+from flask.ext.restful import Resource, abort
+
+from vegadns.api.config import config
+from vegadns.api import endpoint
+from vegadns.api.endpoints import AbstractEndpoint
+from vegadns.api.models.apikey import ApiKey
+from vegadns.api.models.account import Account
+from vegadns.api.models.oauth_access_token import OauthAccessToken
+
+from peewee import MySQLDatabase, Model
+from lib.shortcuts import model_to_dict
+
+
+@endpoint
+class Token(AbstractEndpoint):
+    auth_required = False
+    route = '/token'
+
+    def post(self):
+        # FIXME - add rate limiting
+
+        # validate request
+        grant_type = request.form.get('grant_type', '')
+        if grant_type == "" or request.authorization is None:
+            abort(400, message="invalid_request")
+        if grant_type != 'client_credentials':
+            abort(400, message="unsupported_grant_type")
+        if request.authorization is None:
+            abort(400, message="invalid_request")
+
+        # validate client (apikey)
+        key = request.authorization.username
+        secret = request.authorization.password
+
+        now = int(time.time())
+        expire_time = int(config.get('oauth', 'token_expire_time'))
+
+        try:
+            apikey = self.get_apikey(key, secret)
+        except peewee.DoesNotExist:
+            print 'here'
+            abort(400, message="invalid_client")
+
+        try:
+            # look up existing token first, make sure it's good for at least
+            # ten minutes
+            token = self.get_token(apikey.apikey_id, now + 600)
+        except peewee.DoesNotExist:
+            # create one if needed
+            token = OauthAccessToken()
+            token.account_id = apikey.account_id.account_id
+            token.apikey_id = apikey.apikey_id
+            token.access_token = str(uuid.uuid4())
+            token.grant_type = 'client_credentials'
+            token.expires_at = now + expire_time
+            try:
+                token.save()
+            except:
+                abort(500, 'unable to generate token')
+
+        return {
+            'access_token': token.access_token,
+            'token_type': 'bearer',
+            'expires_in': token.expires_at - now
+        }
+
+    def get_token(self, apikey_id, expires_at):
+        return OauthAccessToken().get(
+                OauthAccessToken.apikey_id == apikey_id,
+                OauthAccessToken.expires_at > expires_at
+            )
+
+    def get_apikey(self, key, secret):
+        return ApiKey().select(ApiKey, Account).join(
+                Account,
+                on=ApiKey.account_id == Account.account_id
+        ).where(
+            ApiKey.key == key,
+            ApiKey.secret == secret,
+            ApiKey.deleted == 0,
+            Account.status == 'active'
+        ).get()
