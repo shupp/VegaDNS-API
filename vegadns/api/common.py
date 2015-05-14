6bf6db9aedac948803618bd974f98132f039427e
Bill Shupp <hostmaster@shupp.org>
2015-05-13 19:00:31 -0700

    First pass at adding oauth 4.4 authentication

--- vegadns/api/common.py
+++ vegadns/api/common.py
@@ -1,36 +1,78 @@
 import hashlib
+import re
+import time
+
+import peewee
 
 from vegadns.api.models.account import Account
+from vegadns.api.models.oauth_access_token import OauthAccessToken
 
 
 class Auth(object):
     def __init__(self, request, endpoint):
         self.account = None
-        self.authenticate(request, endpoint)
+        self.request = request
+        self.endpoint = endpoint
+        self.authenticate()
 
-    def authenticate(self, request, endpoint):
-        if endpoint.auth_required is False:
+    def authenticate(self):
+        if self.endpoint.auth_required is False:
             return True
+
+        # determine auth
+        auth_header = self.request.headers['Authorization']
+        p = re.compile('^Bearer[ ]+(.*$)')
+        match = p.findall(auth_header)
+        if match:
+            return self.oauth_authenticate(match[0])
         else:
-            # basic auth for now
-            if request.authorization is None:
-                raise AuthException('Invalid username or password')
+            return self.basic_authenticate()
 
-            email = request.authorization.username
-            password = request.authorization.password
+    def oauth_authenticate(self, token):
+        account = self.get_account_by_oauth_token(token)
+        self.account = account
 
-            account = self.get_account_by_email(email)
-            hashed_pass = hashlib.md5(password).hexdigest()
+    def basic_authenticate(self):
+        # basic auth for now
+        if self.request.authorization is None:
+            raise AuthException('Invalid username or password')
 
-            if account.password != hashed_pass:
-                raise AuthException('Invalid email or password')
+        email = self.request.authorization.username
+        password = self.request.authorization.password
 
-            self.account = account
+        account = self.get_account_by_email(email)
+        hashed_pass = hashlib.md5(password).hexdigest()
+
+        if account.password != hashed_pass:
+            raise AuthException('Invalid email or password')
+
+        self.account = account
 
     def get_account_by_email(self, email):
         try:
-            return Account.get(Account.email == email)
-        except Account.DoesNotExist:
+            return Account.get(
+                Account.email == email,
+                Account.status == 'active'
+            )
+        except peewee.DoesNotExist:
+            raise AuthException('Account not found')
+
+    def get_account_by_oauth_token(self, token):
+        now = int(time.time())
+        try:
+            access_token = OauthAccessToken.get(
+                OauthAccessToken.access_token == token,
+                OauthAccessToken.expires_at > now
+            )
+        except peewee.DoesNotExist:
+            raise AuthException('invalid_token')
+
+        try:
+            return Account.get(
+                Account.account_id == access_token.account_id,
+                Account.status == 'active'
+            )
+        except peewee.DoesNotExist:
             raise AuthException('Account not found')
 
 
