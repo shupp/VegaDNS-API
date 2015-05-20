0b00dab4fc2c3fa3c1f7e31a0ab69755528a639d
Bill Shupp <hostmaster@shupp.org>
2015-05-20 14:23:43 -0700

    Enable debug and profiling optionally via DEBUG and PROFILE environment variables

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
+import os
+
 from vegadns.api import endpoint, app
 from vegadns.api.endpoints import domain, domains, record, records, export
 from vegadns.api.endpoints import apikeys, apikey
 from vegadns.api.endpoints import token
 
+profile = os.environ.get('PROFILE', None)
+debug = bool(os.environ.get('DEBUG', None))
+
+if profile:
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.config['PROFILE'] = True
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug)
