76d97aa52acdbcb63ff7977bc3a5ef2cfd18fa4d
Bill Shupp <hostmaster@shupp.org>
2015-06-02 17:09:50 -0700

    Added create_tables.sql and data.sql to seed data for new installs.  Will improve this later.

---
+++ sql/create_tables.sql
@@ -0,0 +1,206 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account_group_map`
+--
+
+DROP TABLE IF EXISTS `account_group_map`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account_group_map` (
+  `map_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `is_admin` int(11) NOT NULL,
+  PRIMARY KEY (`map_id`),
+  UNIQUE KEY `account_group_map_account_id_group_id` (`account_id`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `cid` int(10) NOT NULL AUTO_INCREMENT,
+  `gid` int(10) DEFAULT '0',
+  `Email` varchar(60) NOT NULL DEFAULT '',
+  `Password` varchar(34) NOT NULL DEFAULT '',
+  `First_Name` varchar(20) NOT NULL DEFAULT '',
+  `Last_Name` varchar(20) NOT NULL DEFAULT '',
+  `Phone` varchar(15) NOT NULL DEFAULT '',
+  `Account_Type` enum('user','group_admin','senior_admin') NOT NULL DEFAULT 'user',
+  `Status` enum('active','inactive') NOT NULL DEFAULT 'inactive',
+  UNIQUE KEY `cid` (`cid`,`Email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `api_keys`
+--
+
+DROP TABLE IF EXISTS `api_keys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `api_keys` (
+  `apikey_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `secret` varchar(255) NOT NULL,
+  `date_created` int(11) NOT NULL,
+  `deleted` int(11) NOT NULL,
+  PRIMARY KEY (`apikey_id`),
+  UNIQUE KEY `api_keys_key` (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `default_records`
+--
+
+DROP TABLE IF EXISTS `default_records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `default_records` (
+  `record_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_owner_id` int(11) DEFAULT NULL,
+  `host` varchar(100) NOT NULL DEFAULT '',
+  `type` char(1) DEFAULT NULL,
+  `val` varchar(512) DEFAULT NULL,
+  `distance` int(4) DEFAULT '0',
+  `weight` int(4) DEFAULT NULL,
+  `port` int(4) DEFAULT NULL,
+  `ttl` int(11) NOT NULL DEFAULT '86400',
+  `default_type` enum('system','group') NOT NULL DEFAULT 'system',
+  UNIQUE KEY `records_id` (`record_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `domain_group_map`
+--
+
+DROP TABLE IF EXISTS `domain_group_map`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domain_group_map` (
+  `map_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `domain_id` int(11) NOT NULL,
+  `permissions` int(11) NOT NULL,
+  PRIMARY KEY (`map_id`),
+  UNIQUE KEY `domain_group_map_domain_id_group_id` (`domain_id`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `domains`
+--
+
+DROP TABLE IF EXISTS `domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domains` (
+  `domain_id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain` varchar(100) NOT NULL DEFAULT '',
+  `owner_id` int(11) DEFAULT NULL,
+  `group_owner_id` int(11) DEFAULT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'inactive',
+  KEY `domain_id` (`domain_id`,`domain`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`group_id`),
+  UNIQUE KEY `groups_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `domain_id` int(11) NOT NULL DEFAULT '0',
+  `cid` int(11) NOT NULL DEFAULT '0',
+  `Email` varchar(60) NOT NULL DEFAULT '',
+  `Name` varchar(60) NOT NULL DEFAULT '',
+  `entry` varchar(200) NOT NULL DEFAULT '',
+  `time` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_access_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_access_tokens` (
+  `access_token_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `apikey_id` int(11) NOT NULL,
+  `access_token` varchar(36) NOT NULL,
+  `grant_type` varchar(20) NOT NULL,
+  `expires_at` int(11) NOT NULL,
+  PRIMARY KEY (`access_token_id`),
+  UNIQUE KEY `oauth_access_tokens_access_token` (`access_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `records`
+--
+
+DROP TABLE IF EXISTS `records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `records` (
+  `domain_id` int(11) NOT NULL DEFAULT '0',
+  `record_id` int(11) NOT NULL AUTO_INCREMENT,
+  `host` varchar(100) NOT NULL DEFAULT '',
+  `type` char(1) DEFAULT NULL,
+  `val` varchar(512) DEFAULT NULL,
+  `distance` int(4) DEFAULT '0',
+  `weight` int(4) DEFAULT NULL,
+  `port` int(4) DEFAULT NULL,
+  `ttl` int(11) NOT NULL DEFAULT '86400',
+  UNIQUE KEY `records_id` (`record_id`),
+  KEY `records_idx` (`domain_id`,`record_id`,`host`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `accounts` VALUES (1,0,'test@test.com','098f6bcd4621d373cade4e832627b4f6','Test','User','','senior_admin','active');
+INSERT INTO `api_keys` VALUES (1,1,'','6d145840921dabcc85907bff35e607289abdad04b7900196ee45f5a4e12ac369','b1163b6387318dbfebaca5740ddb024ad61fa18831bb887ea085036f8df9c180',1431561399,0);
+INSERT INTO `default_records` VALUES (1,0,'hostmaster.DOMAIN:ns1.myserver.com','S','16384:2048:1048576:2560',0,'','',86400,'system');
