fce8b83f887fb55c8fd3fba05b31a59a2ca3c57e
Bill Shupp <hostmaster@shupp.org>
2015-06-09 16:09:10 -0700

    * Added validate method on AccountGroupMap model * Added helper method "format_member" that endpoints can share

--- vegadns/api/models/account_group_map.py
+++ vegadns/api/models/account_group_map.py
@@ -9,6 +9,19 @@ class AccountGroupMap(BaseModel):
     group_id = IntegerField()
     is_admin = IntegerField(default=0)
 
+    def validate(self):
+        if self.is_admin != 0 and self.is_admin != 1:
+            raise ValueError("is_admin must be 1 or 0")
+
+    # helper method for formatting API output
+    def format_member(self, map, account):
+        return {
+            'member_id': map.map_id,
+            'group_id': map.group_id,
+            'is_admin': map.is_admin,
+            'account': account.to_clean_dict()
+        }
+
     class Meta:
         db_table = 'account_group_map'
         indexes = (
