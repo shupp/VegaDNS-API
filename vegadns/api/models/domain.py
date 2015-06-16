408e463d13960991e737411735290668d897000f
Bill Shupp <hostmaster@shupp.org>
2015-06-16 15:35:25 -0700

    Use _id suffix in domain models to be consistent

--- vegadns/api/models/domain.py
+++ vegadns/api/models/domain.py
@@ -6,12 +6,12 @@ from vegadns.api.models import database, BaseModel
 class Domain(BaseModel):
     domain = CharField()
     domain_id = IntegerField(primary_key=True)
-    group_owner = IntegerField(db_column='group_owner_id', null=True)
-    owner = IntegerField(db_column='owner_id', null=True)
+    group_owner_id = IntegerField(db_column='group_owner_id', null=True)
+    owner_id = IntegerField(db_column='owner_id', null=True)
     status = CharField()
 
     class Meta:
         db_table = 'domains'
 
     # For removing unused group_owner field via self.to_clean_dict()
-    clean_keys = ["group_owner"]
+    clean_keys = ["group_owner_id"]
