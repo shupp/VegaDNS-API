947e98672b73392a4fb637c5ef123ce473a68c89
Bill Shupp <hostmaster@shupp.org>
2015-06-17 09:32:00 -0700

    Added create domain support, still need default records support

--- vegadns/api/endpoints/domains.py
+++ vegadns/api/endpoints/domains.py
@@ -1,5 +1,7 @@
-from flask import Flask, abort, redirect, url_for
+from flask import Flask, abort, request
 from flask.ext.restful import Resource, Api, abort
+
+import peewee
 
 from vegadns.api import endpoint
 from vegadns.api.endpoints import AbstractEndpoint
@@ -19,6 +21,41 @@ class Domains(AbstractEndpoint):
         except:
             abort(404, message="no domains found")
         return {'status': 'ok', 'domains': domains}
+
+    def post(self):
+        domain = request.form.get("domain")
+        if domain is None:
+            abort(400, message="domain parameter is required")
+
+        # check for duplicate first
+        try:
+            ModelDomain.get(ModelDomain.domain == domain)
+            abort(400, message="Domain already exists")
+        except peewee.DoesNotExist:
+            pass
+
+        model = ModelDomain()
+        model.domain = domain
+        if self.auth.account.account_type != 'senior_admin':
+            model.status = 'inactive'
+            model.owner_id = self.auth.account.account_id
+
+        try:
+            model.save()
+        except ValueError:
+            abort(400, message="Invalid parameters")
+        except peewee.IntegrityError:
+            # race condition, unique constraint will catch it
+            abort(400, message="Domain already exists")
+
+        # add records
+            # model.add_default_records()
+
+        if self.auth.account.account_type != 'senior_admin':
+            # send email to admins
+            pass
+
+        return {'status': 'ok', 'domain': model.to_clean_dict()}, 201
 
     def get_domain_list(self):
         if self.auth.account.account_type == 'senior_admin':
--- vegadns/api/models/domain.py
+++ vegadns/api/models/domain.py
@@ -1,17 +1,27 @@
-from peewee import CharField, IntegerField
+from peewee import CharField, IntegerField, PrimaryKeyField
 
 from vegadns.api.models import database, BaseModel
+from vegadns.validate.dns import ValidateDNS
 
 
 class Domain(BaseModel):
-    domain = CharField()
-    domain_id = IntegerField(primary_key=True)
-    group_owner_id = IntegerField(db_column='group_owner_id', null=True)
-    owner_id = IntegerField(db_column='owner_id', null=True)
-    status = CharField()
+    domain = CharField(unique=True)
+    domain_id = PrimaryKeyField()
+    group_owner_id = IntegerField(
+        db_column='group_owner_id', null=True, default=0
+    )
+    owner_id = IntegerField(db_column='owner_id', null=True, default=0)
+    status = CharField(default="active")
 
     class Meta:
         db_table = 'domains'
 
     # For removing unused group_owner field via self.to_clean_dict()
     clean_keys = ["group_owner_id"]
+
+    def validate(self):
+        if self.status not in ["active", "inactive"]:
+            raise ValueError("status must be either 'active' or 'inactive'")
+
+        if not ValidateDNS.record_hostname(self.domain):
+            raise ValueError("domain is invalid: " + self.domain)
