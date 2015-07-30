dd565c8c8b8f26c0918cb1fe914e6e90a0f65e6c
Bill Shupp <hostmaster@shupp.org>
2015-07-29 17:31:21 -0700

    * Added support for multiple template handling in config.py via argument * Added trusted_ips config support via TRUSTED_IPS environment

--- docker/templates/config.py
+++ docker/templates/config.py
@@ -18,13 +18,21 @@ def main():
             "db_pass": os.getenv("SECRET_DB_PASS", default="secret"),
             "db_db": os.getenv("DB_DB", default="vegadns"),
             "vegadns_generation": os.getenv("VEGADNS_GENERATION", default=""),
+            "vegadns": os.getenv("VEGADNS", default="http://127.0.0.1/1.0/export/tinydns"),
+            "trusted_ips": os.getenv("TRUSTED_IPS", default="127.0.0.1"),
         }
     except Exception as err:
         print "Problem reading environment", err
         sys.exit(1)
 
+    # optionally use first argument as template, path is still
+    # relative to this script
+    template_file = "./local.ini.template"
+    if len(sys.argv) > 1:
+        template_file = sys.argv[1]
+
     try:
-        with open(directory + "/local.ini.template") as template:
+        with open(directory + "/" + template_file) as template:
             print pystache.render(template.read(), config)
     except Exception as err:
         print "Problem rendering template", err
