cc93d98b513bee811ac83f4a42fa43caa938fe75
Bill Shupp <hostmaster@shupp.org>
2015-08-05 11:59:34 -0700

    Added generic CORS support for upcoming JS client

--- vegadns/api/__init__.py
+++ vegadns/api/__init__.py
@@ -4,9 +4,11 @@ import os
 
 from flask import Flask
 from flask.ext.restful import Resource, Api
+from flask.ext.cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 if not bool(os.environ.get('DEBUG', None)):
