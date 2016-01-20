929bc198f503a840d3eb25203046b386b6df72b9
Bill Shupp <hostmaster@shupp.org>
2016-01-20 15:42:48 -0800

    Initial smtp ssl/tls support, not yet tested

--- vegadns/api/email/smtp.py
+++ vegadns/api/email/smtp.py
@@ -7,7 +7,34 @@ from vegadns.api.email.common import Common
 class SMTP(Common):
     def send(self, to, subject, body, extra_headers):
         # FIXME extra_headers
-        server = smtplib.SMTP('localhost')
+        smtp_host = config.get("email", "smtp_host")
+        smtp_port = config.get("email", "smtp_port")
+
+        smtp_keyfile = None
+        if len(config.get("email", "smtp_keyfile")):
+            smtp_keyfile = config.get("email", "smtp_keyfile")
+
+        smtp_certfile = None
+        if len(config.get("email", "smtp_certfile")):
+            smtp_certfile = config.get("email", "smtp_certfile")
+
+        if config.get('email', 'smtp_ssl') in ["True", "true"]:
+            server = smtplib.SMTP_SSL(
+                smtp_host,
+                smtp_port,
+                None,
+                smtp_keyfile,
+                smtp_certfile
+            )
+        else:
+            server = smtplib.SMTP(
+                smtp_host,
+                smtp_port
+            )
+
+            if config.get("email", "smtp_tls") in ["True", "true"]:
+                server.starttls(smtp_keyfile, smtp_certfile)
+
         if smtp_auth in ["True", "true"]:
             server.login(
                 config.get('email', 'smtp_user'),
