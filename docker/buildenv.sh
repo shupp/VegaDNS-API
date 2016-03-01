adf18cfd1fe34a1eb0f7c2cc6b22f793f64422db
Bill Shupp <hostmaster@shupp.org>
2016-03-01 12:23:28 -0800

    Fixed some bcrypt dependencies
    * cffi build depends on libffi-dev
    * Terminate buildenv.sh script on error, show commands for debugging
    * Upgrade pip to be safe

--- docker/buildenv.sh
+++ docker/buildenv.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # build environment
 rm -rf /var/www/vegadns2/venv
 virtualenv /var/www/vegadns2/venv
 source /var/www/vegadns2/venv/bin/activate
+pip install --upgrade pip
 pip install -r /var/www/vegadns2/requirements.txt
 pip install uwsgi
