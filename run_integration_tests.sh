81a550fde5946b831548f6300336a5abe3af4449
Bill Shupp <bill@shupp.org>
2017-03-30 11:08:43 -0700

    More work on integration test automation.  Should now avoid naming and host port collisions.
    * Use timestamp based COMPOSE_PROJECT_NAME environment
    * Moved to unique compose file which skips ui and disables ports
    * Remove initial stop containers
    * Update run file accordingly
    * Remove cli and api images at end

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+DATE=`date +%s`
+export COMPOSE_PROJECT_NAME="vegadnsapiintegration${DATE}"
+
 printf "Building VegaDNS-CLI ...\n"
-docker build -t vegadns-cli https://github.com/shupp/VegaDNS-CLI.git
+docker build -t ${COMPOSE_PROJECT_NAME}_vegadns-cli https://github.com/shupp/VegaDNS-CLI.git
 
-printf "\nStopping any existing containers ...\n"
-API_URL=${API_URL:-http://localhost:5000} docker-compose down || exit 1
-
-printf "\nStarting fresh ones\n"
-API_URL=${API_URL:-http://localhost:5000} docker-compose up -d || exit 1
+printf "\nStarting containers\n"
+API_URL=${API_URL:-http://localhost:5000} docker-compose -f docker-compose-integration-test.yml up -d || exit 1
 
 printf "\nwaiting for mysql "
 RETURN_CODE=1
 while [ $RETURN_CODE -ne 0 ]; do
-    docker exec -i vegadnsapi_mysql_1 mysql -h mysql -u vegadns -psecret -e QUIT vegadns >/dev/null 2>&1
+    docker exec -i ${COMPOSE_PROJECT_NAME}_mysql_1 mysql -h mysql -u vegadns -psecret -e QUIT vegadns >/dev/null 2>&1
     RETURN_CODE=$?
     sleep 1
     printf "."
@@ -23,11 +23,20 @@ printf " done\n"
 set -e
 
 printf "\nSeeding data ...\n"
-docker exec -i vegadnsapi_mysql_1 mysql -h mysql -u vegadns -psecret vegadns < ./sql/create_tables.sql
-docker exec -i vegadnsapi_mysql_1 mysql -h mysql -u vegadns -psecret vegadns < ./sql/data.sql
+docker exec -i ${COMPOSE_PROJECT_NAME}_mysql_1 mysql -h mysql -u vegadns -psecret vegadns < ./sql/create_tables.sql
+docker exec -i ${COMPOSE_PROJECT_NAME}_mysql_1 mysql -h mysql -u vegadns -psecret vegadns < ./sql/data.sql
 
 printf "\nRunning integration tests ...\n"
-docker run -e NAMESERVER=client -e HOST=http://api:5000 --net vegadnsapi_vegadns_net vegadns-cli
+docker run \
+    -e NAMESERVER=client \
+    -e HOST=http://api:5000 \
+    --net ${COMPOSE_PROJECT_NAME}_vegadns_net \
+    --name ${COMPOSE_PROJECT_NAME}_vegadns-cli \
+    ${COMPOSE_PROJECT_NAME}_vegadns-cli
 
 printf "\nCleaning up ...\n"
-API_URL=${API_URL:-http://localhost:5000} docker-compose down
+docker rm ${COMPOSE_PROJECT_NAME}_vegadns-cli
+docker rmi ${COMPOSE_PROJECT_NAME}_vegadns-cli
+API_URL=${API_URL:-http://localhost:5000} docker-compose -f docker-compose-integration-test.yml down
+# Need to manually remove this one, not sure why
+docker rmi ${COMPOSE_PROJECT_NAME}_api
