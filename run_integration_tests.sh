ca8e3304204684408a91f11fa7f9331d38662ef4
Bill Shupp <bill@shupp.org>
2017-03-30 09:57:24 -0700

    Moved run_integration_tests.sh from echo printf

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-echo "Building VegaDNS-CLI ..."
+printf "Building VegaDNS-CLI ...\n"
 docker build -t vegadns-cli https://github.com/shupp/VegaDNS-CLI.git
 
-echo
-echo "Stopping any existing containers ..."
+printf "\nStopping any existing containers ...\n"
 API_URL=${API_URL:-http://localhost:5000} docker-compose down || exit 1
 
-echo
-echo "Starting fresh ones"
+printf "\nStarting fresh ones\n"
 API_URL=${API_URL:-http://localhost:5000} docker-compose up -d || exit 1
 
-echo
-echo -n "waiting for mysql "
+printf "\nwaiting for mysql "
 RETURN_CODE=1
 while [ $RETURN_CODE -ne 0 ]; do
     docker exec -i vegadnsapi_mysql_1 mysql -h mysql -u vegadns -psecret -e QUIT vegadns >/dev/null 2>&1
     RETURN_CODE=$?
     sleep 1
-    echo -n .
+    printf "."
 done
-echo " done"
+printf " done\n"
 
 # exit on failure
 set -e
 
-echo
-echo "Seeding data ..."
+printf "\nSeeding data ...\n"
 docker exec -i vegadnsapi_mysql_1 mysql -h mysql -u vegadns -psecret vegadns < ./sql/create_tables.sql
 docker exec -i vegadnsapi_mysql_1 mysql -h mysql -u vegadns -psecret vegadns < ./sql/data.sql
 
-echo
-echo "Running integration tests ..."
+printf "\nRunning integration tests ...\n"
 docker run -e NAMESERVER=client -e HOST=http://api:5000 --net vegadnsapi_vegadns_net vegadns-cli
 
-echo
-echo "Cleaning up ..."
+printf "\nCleaning up ...\n"
 API_URL=${API_URL:-http://localhost:5000} docker-compose down
