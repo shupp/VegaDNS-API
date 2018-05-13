shupp/VegaDNS-API

commit a643fc6eeffdd38382c28133228446761f231a82

Clean up old oauth access tokens

vegadns/api/common.py
@@ -1,5 +1,6 @@
 import re
 import time
+import random
 
 from flask import request
 from netaddr import IPSet
@@ -84,6 +85,15 @@ class Auth(object):
 
     def get_account_by_oauth_token(self, token):
         now = int(time.time())
+
+        # First, remove old tokens every 4 requests or so
+        if random.randint(1,4) == 1:
+            q = OauthAccessToken.delete().where(
+                OauthAccessToken.expires_at < now
+            )
+            deleted = q.execute()
+            # print "Old tokens deleted: " + str(deleted)
+
         try:
             access_token = OauthAccessToken.get(
                 OauthAccessToken.access_token == token,

commit 45820a53c9334c7507d69dfbc4d352307a46c2b6

Fixed pep8 error introduced in a643fc6e

vegadns/api/common.py
@@ -87,7 +87,7 @@ class Auth(object):
         now = int(time.time())
 
         # First, remove old tokens every 4 requests or so
-        if random.randint(1,4) == 1:
+        if random.randint(1, 4) == 1:
             q = OauthAccessToken.delete().where(
                 OauthAccessToken.expires_at < now
             )